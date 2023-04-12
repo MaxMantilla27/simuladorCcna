b9b610b743e70583bdcdc7f1e84b20f6da893f65
Rodrigomoll <victor.mollocondo@tecsup.edu.pe>
2023-04-12 18:07:50 -0500

    llamarObtenerPorcentaje

--- src/app/auth/ModoEntrenamiento/modo-entrenamiento.component.ts
+++ src/app/auth/ModoEntrenamiento/modo-entrenamiento.component.ts
@@ -64,7 +64,8 @@ export class ModoEntrenamientoComponent implements OnInit {
     this.ListaExamenesIncompletos();
     this.ListaExamenesConcluidos();
     this.ListaExamenesPorModo();
-    this.ObtenerPromedioDominioPorModo()
+    this.ObtenerPromedioDominioPorModo();
+    this.ObtenerPorcentaje();
   }
   RegistrarExamen(){
     if(this.userForm.valid){
